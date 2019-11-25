c537abe901b5068c6ab403f9f1dc54530f7f0642
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-25 10:53:11 -0700

    fixed typo

---
+++ front-end/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import GlobalStyle from './theme/GlobalStyle';
+import Home from './pages/Home';
+import Topics from './pages/Topics';
+import Topic from './pages/Topic';
+import Favorites from './pages/Favorites';
+import Navbar from './Components/Navbar';
+
+function App(props) {
+  return (
+    <div>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Topics" component={Topics} />
+        <Route exact path="/Favorites" component={Favorites} />
+        <Route exact path="/Topic" component={Topic} />
+
+      </Switch>
+    </div>
+  )
+}
+
+export default App;
