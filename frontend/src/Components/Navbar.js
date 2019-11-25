9324929ba9500059088a613749e66799566799f5
Zach McEntire <mcentirezach@gmail.com>
2019-11-25 14:47:17 -0700

    Navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+import {Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse} from 'reactstrap';
 
-const NavbarWrapper = styled.div``;
+// const NavbarWrapper = styled.div``;
 
-function Navbar(props) {
+const Navbar = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-    <NavbarWrapper>
-
-    </NavbarWrapper>
+    <div>
+      <Navbar color="faded" dark>
+        <NavbarBrand href="/" className="mr-auto">Home</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/Topics">Topics</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Favorites">Favorites</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Topic">Topic</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   )
 }
 
