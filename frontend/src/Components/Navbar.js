de51f1a4defa583a0ecdaad2617057259dd64728
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-25 10:23:26 -0700

    renamed front-end to frontend

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React form 'react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div``;
+
+function Navbar(props) {
+  return (
+    <NavbarWrapper>
+
+    </NavbarWrapper>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Favorites.js
@@ -0,0 +1,14 @@
+import React form 'react';
+import styled from 'styled-components';
+
+const FavoritesWrapper = styled.div``;
+
+function Favorites(props) {
+  return (
+    <FavoritesWrapper>
+
+    </FavoritesWrapper>
+  )
+}
+
+export default Favorites;
