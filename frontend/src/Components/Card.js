80a60b6ff2cce92e413a5661f305ef1e5381cc2a
Justin Reidy <justintreidy@gmail.com>
2019-11-26 11:52:04 -0700

    added Topics page with Axios call

---
+++ frontend/src/Components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Card(props){
+    return(
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+export default Card
