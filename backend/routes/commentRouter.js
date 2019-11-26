958590be0d1fd25eea8b028fcd1e3ce0122e1895
Stephen Presley <stephen.presley@live.com>
2019-11-26 09:42:04 -0700

    added topic model and router

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -2,51 +2,50 @@ const express = require('express')
 const commentRouter = express.Router()
 const Comment = require('../models/Comment')
 
-
 commentRouter.route('/:postID')
 
-    .get((req, res) =>{
-        Comment.find({post: req.params.postID}, (err, comments) =>{
-            if(err) return res.status(500).send(err)
+    .get((req, res) => {
+        Comment.find({ post: req.params.postID }, (err, comments) => {
+            if (err) return res.status(500).send(err)
             return res.status(200).send(comments)
         })
     })
 
-    .post((req, res) =>{
+    .post((req, res) => {
         req.body.post = req.params.postID
         const newComment = new Comment(req.body)
-        newComment.save((err, comment) =>{
-            if(err) return res.status(500).send(err)
+        newComment.save((err, comment) => {
+            if (err) return res.status(500).send(err)
             return res.status(201).send(comment)
         })
     })
 
 commentRouter.route('/:_id')
 
-    .get((req, res) =>{
-        comment.findById(req.params._id, (err, comment) =>{
-            if(err) return res.status(500).send(err)
+    .get((req, res) => {
+        comment.findById(req.params._id, (err, comment) => {
+            if (err) return res.status(500).send(err)
             return res.status(200).send(comment)
         })
     })
 
-    .put((req, res) =>{
+    .put((req, res) => {
         comment.findOneAndUpdate(
-            {_id: req.params._id},
+            { _id: req.params._id },
             req.body,
-            {new: true},
-            (err, comment) =>{
-                if(err) return res.status(500).send(err)
+            { new: true },
+            (err, comment) => {
+                if (err) return res.status(500).send(err)
                 return res.status(200).send(comment)
             }
         )
     })
 
-    .delete((req, res) =>{
+    .delete((req, res) => {
         comment.findOneAndDelete(
-            {_id: req.params._id},
-            (err, comment) =>{
-                if(err) return res.status(500).send(err)
+            { _id: req.params._id },
+            (err, comment) => {
+                if (err) return res.status(500).send(err)
                 return res.status(200).send(comment)
             }
         )
--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -2,49 +2,50 @@ const express = require('express')
 const postRouter = express.Router()
 const Post = require('../models/Post')
 
-postRouter.route('/')
+postRouter.route('/:topicID')
 
     .get((req, res) => {
-        Post.find((err, posts) =>{
-            if(err) return res.status(500).send(err)
+        Post.find({ topic: req.params.topicID }, (err, posts) => {
+            if (err) return res.status(500).send(err)
             return res.status(200).send(posts)
         }).populate('comments')
     })
 
     .post((req, res) => {
+        req.body.topic = req.params.topicID
         const newPost = new Post(req.body)
-        newPost.save((err, post) =>{
-            if(err) return res.status(500).send(err)
+        newPost.save((err, post) => {
+            if (err) return res.status(500).send(err)
             return res.status(201).send(post)
         })
     })
 
-postRouter.route('/:_id')
+postRouter.route('/:postID')
 
     .get((req, res) => {
-        Post.findById(req.params._id, (err, post) =>{
-            if(err) return res.status(500).send(err)
+        Post.findById({post: req.params.postID}, (err, post) => {
+            if (err) return res.status(500).send(err)
             return res.status(200).send(post)
         }).populate('comments')
     })
 
-    .put((req, res) =>{
+    .put((req, res) => {
         Post.findOneAndUpdate(
-            {_id: req.params._id},
+            { _id: req.params._id },
             req.body,
-            {new: true},
-            (err, post) =>{
-                if(err) return res.status(500).send(err)
+            { new: true },
+            (err, post) => {
+                if (err) return res.status(500).send(err)
                 return res.status(200).send(post)
             }
         ).populate('comments')
     })
 
-    .delete((req, res) =>{
+    .delete((req, res) => {
         Post.findOneAndDelete(
-            {_id: req.params._id},
-            (err, post) =>{
-                if(err) return res.status(500).send(err)
+            { _id: req.params._id },
+            (err, post) => {
+                if (err) return res.status(500).send(err)
                 return res.status(200).send(post)
             }
         )
