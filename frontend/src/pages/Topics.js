56cf114b230116afc4a9acfdb27fbdc0ab58e273
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-27 09:28:18 -0700

    house cleaning

--- frontend/src/pages/Topics.js
+++ frontend/src/pages/Topics.js
@@ -5,21 +5,21 @@ import Card from '../Components/Card'
 
 class TopicsContainer extends Component {
 
-    componentDidMount(){
-      this.props.getTopics();
-    };
+  componentDidMount(){
+    this.props.getTopics();
+  };
 
-    render() {
-        const mappedTopics = this.props.topics.map(topic => (
-            <Card key={topic._id}
-                  title={topic.name}/>
-        ))
-        return (
-            <div>
-                {mappedTopics}
-            </div>
-        );
-    }
-}
+  render() {
+    const mappedTopics = this.props.topics.map(topic => (
+      <Card key={topic._id}
+            title={topic.name}/>
+    ));
+    return (
+      <div>
+        {mappedTopics}
+      </div>
+    );
+  };
+};
 
 export default withPosts(TopicsContainer);
