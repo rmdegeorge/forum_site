3e3da6c56b0a89a1db8103c2e20d4f0b83a024e0
Rob DeGeorge <rmdegeorge@gmail.com>
2019-11-27 16:44:00 -0700

    cleaned up code and converted from class to functional component

--- frontend/src/Components/BackButton.js
+++ frontend/src/Components/BackButton.js
@@ -1,26 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Button } from 'reactstrap';
 import {withPosts} from '../providers/PostDataProvider';
 
 
-class BackButton extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-
-        }
-        
-    }
-    
-
-
-    render(){
-    return (
-        <div>
-            <Button color="secondary" onClick={this.props.goBack}>Back</Button>
-        </div>
-    )}
-}
-
+function BackButton(props) {
+  return (
+    <div>
+      <Button color="secondary" onClick={props.goBack}>Back</Button>
+    </div>
+  );
+};
 export default withPosts(BackButton)
