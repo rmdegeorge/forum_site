74156b32269665d0363f5b5ac75a1fa24dc30980
Justin Reidy <justintreidy@gmail.com>
2019-11-27 12:26:08 -0700

    Added Topic selection dropdown on post form

--- frontend/src/Components/PostForm.js
+++ frontend/src/Components/PostForm.js
@@ -2,31 +2,55 @@ import React, {Component} from 'react'
 import {withPosts} from '../providers/PostDataProvider'
 import styled from 'styled-components'
 
+const FormWrapper = styled.div`
+    display: grid
+    grid-template-columns: none
+    
+`
+
 class PostForm extends Component{
     constructor(props){
         super(props)
 
         this.state = {
-
+            _id: ""
         }
     }
 
+    componentDidMount(){
+        this.props.getTopics();
+    }
+
     render(){
+        
+        const mappedTopics = this.props.topics.map(topic => (
+            <option value={topic._id}>{topic.name}</option>
+        ))
+
+        const handleChange = e => {
+            this.setState({_id: e.target.value})
+        }
+
+        
+
         return (
-            <div>
+            <FormWrapper>
 
                 <form>
 
                     {/* Title, username, body, tags */}
                     <input type="text" name="postTitle" id="" placeholder="Title"/>
                     <input type="text" name="postUsername" placeholder="Username"/>
+                    <select onChange={handleChange}>
+                        {mappedTopics}
+                    </select>
                     <textarea name="postBody" id="" cols="30" rows="10" placeholder="Content"></textarea>
                     <input type="text" name="postTags" id="" placeholder="tags"/>
                     <button>Post</button>
 
                 </form>
                 
-            </div>
+            </FormWrapper>
         )
     }
 }
