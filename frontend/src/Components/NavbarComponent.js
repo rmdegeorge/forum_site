0f567303185e6f6f517587d2fe9224f8c7bdb2ae
Zach McEntire <mcentirezach@gmail.com>
2019-12-03 16:02:42 -0700

    search filter for home, topic and topics

--- frontend/src/Components/NavbarComponent.js
+++ frontend/src/Components/NavbarComponent.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
 // import styled from 'styled-components';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse} from 'reactstrap';
-import {MDBFormInline, MDBBtn} from 'mdbreact';
+
 
 // const NavbarWrapper = styled.div``;
 
@@ -10,19 +9,11 @@ const NavbarComponent = (props) => {
   const [collapsed, setCollapsed] = useState(true);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
-
-  
   
   return (
     <div>
       <Navbar color="dark" dark>
         <NavbarBrand href="/" className="mr-auto">Home</NavbarBrand>
-        <MDBFormInline className ="md-form m-0">
-          <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-            <MDBBtn outline color="primary" size="sm" type="submit" className="mr-auto">
-              Search
-            </MDBBtn>
-        </MDBFormInline>
         <NavbarToggler onClick={toggleNavbar} className="mr-2"/>
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MDBCol} from "mdbreact";
 import {withPosts} from '../providers/PostDataProvider';
 
 
@@ -19,19 +20,42 @@ const Title = styled.h1`
 `;
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: ''
+    };
+  }
 
   componentDidMount() {
     this.props.getRandomPosts();
   };
 
+  updateSearch = event => {
+    this.setState({search: event.target.value.substr(0,20)})
+  }
+
   render() {
     const sortedPosts = this.props.allPosts.sort((a, b) => (a.votes > b.votes) ? -1 : 1); // sort all posts by number of votes
-    const displayPopularPosts = sortedPosts.slice(0,9).map((post) =>
+    let filteredPosts = sortedPosts.filter(
+      (post) => {
+        return post.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      }
+    )
+    const displayPopularPosts = filteredPosts.slice(0,9).map((post) =>
       <Post key={post._id} type="popular" postInfo={post} />
     );
     return (
       <HomeWrapper>
         <Title>Top 10 Most Popular Posts</Title>
+        <MDBCol md="6">
+      <div className="input-group md-form form-sm form-1 pl-0">
+        <div className="input-group-prepend">
+        </div>
+        <input className="form-control my-0 py-1" type="text" value={this.state.search} placeholder="Search" aria-label="Search" onChange={this.updateSearch} />
+      </div>
+    </MDBCol>
         {displayPopularPosts}
       </HomeWrapper>
     )
