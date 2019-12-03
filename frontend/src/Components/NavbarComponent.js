60ab458e64c6a505e90e04b3444ff8ec6bc09507
Zach McEntire <mcentirezach@gmail.com>
2019-12-03 11:56:10 -0700

    search button

--- frontend/src/Components/NavbarComponent.js
+++ frontend/src/Components/NavbarComponent.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 // import styled from 'styled-components';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse} from 'reactstrap';
+import {MDBFormInline, MDBBtn} from 'mdbreact';
 
 // const NavbarWrapper = styled.div``;
 
@@ -8,11 +10,19 @@ const NavbarComponent = (props) => {
   const [collapsed, setCollapsed] = useState(true);
 
   const toggleNavbar = () => setCollapsed(!collapsed);
+
+  
   
   return (
     <div>
       <Navbar color="dark" dark>
         <NavbarBrand href="/" className="mr-auto">Home</NavbarBrand>
+        <MDBFormInline className ="md-form m-0">
+          <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+            <MDBBtn outline color="primary" size="sm" type="submit" className="mr-auto">
+              Search
+            </MDBBtn>
+        </MDBFormInline>
         <NavbarToggler onClick={toggleNavbar} className="mr-2"/>
         <Collapse isOpen={!collapsed} navbar>
           <Nav navbar>
@@ -20,18 +30,10 @@ const NavbarComponent = (props) => {
               <NavLink href="/Topics">Topics</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/Favorites">Favorites</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/Topic">Topic</NavLink>
-            </NavItem>
-            <NavItem>
               <NavLink href="/NewPost">New Post</NavLink>
             </NavItem>
             <NavItem>
-              <form>
-                <input type="text" placeholder="search"/>
-              </form>
+              
             </NavItem>
           </Nav>
         </Collapse>
