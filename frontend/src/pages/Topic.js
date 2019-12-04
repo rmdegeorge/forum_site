22924d7c87ec3a21961852d7f2ff9faf0b8984ed
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-04 11:28:41 -0700

    cleaned some things up

--- frontend/src/pages/Topic.js
+++ frontend/src/pages/Topic.js
@@ -13,7 +13,16 @@ const TopicWrapper = styled.div`
   width: 75%;
   margin: 0 auto 0 auto;
 `;
-const TopicTitle = styled.h1``;
+const TopicTitle = styled.h1`
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+`;
+const SerchNewPostWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 10px 0 -5px;
+`;
 
 class Topic extends Component {
   constructor(props) {
@@ -40,14 +49,14 @@ class Topic extends Component {
   updateSearch = event => {
     this.setState({search: event.target.value.substr(0,20)})
   }
- 
+
   render(){
     let filteredPosts = this.props.posts.filter(
       (posts) => {
         return posts.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
       }
     )
-    
+
     const populatePosts = filteredPosts.map((post) => {
       return <Post key={post._id} postInfo={post} />
     });
@@ -57,19 +66,22 @@ class Topic extends Component {
     return (
       <TopicWrapper>
         <BackButton goBack={this.props.history.goBack} />
-        <Link to='/NewPost'>
-          <Button variant="contained">Create New Post</Button>
-        </Link>
+
         <TopicTitle>
           {this.state.topic}
         </TopicTitle>
-        <MDBCol md="6">
-          <div className="input-group md-form form-sm form-1 pl-0">
-            <div className="input-group-prepend">
+        <SerchNewPostWrapper>
+          <MDBCol md="6">
+            <div className="input-group md-form form-sm form-1 pl-0">
+              <div className="input-group-prepend">
+              </div>
+              <input className="form-control my-0 py-1" type="text" value={this.state.search} placeholder="Search" aria-label="Search" onChange={this.updateSearch} />
             </div>
-            <input className="form-control my-0 py-1" type="text" value={this.state.search} placeholder="Search" aria-label="Search" onChange={this.updateSearch} />
-          </div>
-        </MDBCol>
+          </MDBCol>
+          <Link to='/NewPost'>
+            <Button variant="contained">Create New Post</Button>
+          </Link>
+        </SerchNewPostWrapper>
         {populatePosts}
       </TopicWrapper>
     );
