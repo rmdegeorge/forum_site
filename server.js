57be9b38755fa29a970e1a95375417e42595db6c
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-05 10:49:59 -0700

    setting up for deployment with heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const PORT = 8080
+const path = require("path")
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use('/topics', require('./routes/topicRouter'))
 app.use('/Posts', require('./routes/postRouter'))
 app.use('/comments', require('./routes/commentRouter'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 mongoose.connect(
     'mongodb://localhost:27017/forum',
@@ -22,6 +25,10 @@ mongoose.connect(
         console.log(err)
     })
 
-app.listen(PORT, () => {
-    console.log(`app is listening on port ${PORT}`)
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+});
