b0b2d9aa5cc7353c8849d3a0dfe65450a36b1ef5
Rob DeGeorge <rmdegeorge@gmail.com>
2019-12-05 12:43:02 -0700

    fixed CORS error

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const path = require("path")
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(express.json())
 app.use('/topics', require('./routes/topicRouter'))
 app.use('/Posts', require('./routes/postRouter'))
